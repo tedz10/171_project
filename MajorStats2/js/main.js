09aa5dc399c92b54d10f49a1a33187cd832751ea
Ted <ted@Teds-MacBook-Pro-3.local>
2018-11-25 00:25:02 -0800

    added some of the visualizations

--- MajorStats2/js/main.js
+++ MajorStats2/js/main.js
@@ -22,28 +22,6 @@ var configs4 = [{key: "Interceptions", title: "Interceptions Thrown"},
 // var configs4 = ["Interceptions","Picks"];
 
 
-// initialize graph path
-// var yType = "wins";
-// var lineGraph = d3.line()
-//     .x(function(d){ return x(d.Year)})
-//     .y(function(d){return y(d[yType])});
-//
-// var lines_pats=svg.append("path");
-// var lines_lions=svg.append("path");
-// var lines_browns=svg.append("path");
-    // .data(data);
-
-// Initialize data
-// loadData();
-
-// FIFA world cup
-// var data;
-// var pats_data;
-// var lions_data;
-// var browns_data;
-
-//
-
 
 
 // Load CSV file
@@ -86,249 +64,240 @@ var configs4 = [{key: "Interceptions", title: "Interceptions Thrown"},
 
 
 // create a function that makes the tooltip output visually appealing
-function tooltipText(str){
-    var caseFixed = str.substr(0,1) + str.substr(1).toLowerCase();
-    var parts = caseFixed.split("_");
-    console.log(parts);
-    if (parts.length > 1){
-        var results = "";
-        for (var i = 0; i < parts.length; i++){
-            results = results + " " + parts[i];
-        }
-        return results;
-    }
-    return parts;
-}
+// function tooltipText(str){
+//     var caseFixed = str.substr(0,1) + str.substr(1).toLowerCase();
+//     var parts = caseFixed.split("_");
+//     console.log(parts);
+//     if (parts.length > 1){
+//         var results = "";
+//         for (var i = 0; i < parts.length; i++){
+//             results = results + " " + parts[i];
+//         }
+//         return results;
+//     }
+//     return parts;
+// };
 
 
-function updateVis(d){
-    console.log(d.Year)
-    curYear = d.Year;
-    for (var i = 0; i <2; i++) {
-        barcharts1[i].wrangleData()
-        barcharts2[i].wrangleData()
-        barcharts3[i].wrangleData()
-        barcharts4[i].wrangleData()
-    }
-    document.getElementById("specific_name").innerHTML = "Major stats for " + formatDate(d.Year);
-}
+d3.queue()
+    .defer(d3.csv, "data/MajorStats1.csv")
+    .defer(d3.csv, "data/rushing.csv")
+    .defer(d3.csv, "data/strongandpoorlyperformingteams.csv")
+    .await(loadData);
+
+function loadData(error, data, rushingData, turnOverData, treeData) {
+
+
+// 	    // console.log(csv)
+
+
+    data.forEach(function(d){
+        // Convert string to 'date object'
+        d.DefensivePassingYards = +d.DefensivePassingYards;
+        d.DefensiveRushingYards = +d.DefensiveRushingYards;
+        d.OffensivePassingYards = +d.OffensivePassingYards;
+        d.OffensiveRushingYards = +d.OffensiveRushingYards;
+        d.Interceptions = +d.Interceptions;
+        d.Picks = +d.Picks;
+        d.Year = parseDate(d.Year);
+        d.OffensiveTouchdowns = +d.OffensiveTouchdowns;
+        d.TouchdownsScoredAgainst = +d.TouchdownsScoredAgainst;
+        d.wins = +d.wins;
+
+        // Convert numeric values to 'numbers'
+    });
+
+    //******RUSHING BAR CHART******//
+
+    //Create rushing bar chart
+    rushingBar = new Rushing("info4", rushingData);
+
+
+    //******Turnover  CHART******//
+
+    //Create rushing bar chart
+    turnoverBar = new Turnover("info6", turnOverData);
+    console.log("turnoverbar end");
 
 
 
-d3.select("body.p")
-    .append("p")
-    .text("Click on a Team to read more")
-    .attr("id", "instructions");
+}// Margin object with properties for the four directions
+var margin = {top: 20, right: 10, bottom: 40, left: 60};
 
-//initial data
+// SVG Size
 
-totalbuildings = [];
-
-
-
-
-// set up the margin, x, and y
-
-var margin = {top: 20, right: 80, bottom: 40, left: -50},
-    width = 800 - margin.left - margin.right,
+var width = 600 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scaleLinear()
-    .range([0, width]);
 
-var y = d3.scaleBand()
-    .rangeRound([height, 0])
-    .padding(0.1);
+// Load CSV file
+d3.csv("data/OpponentOffensiveYards.csv", function(data){
+
+    // Convert numeric values to numbers
+    data.forEach(function(d) {
+        d.Total_Wins = +d.Total_Wins;
+        d.Offensive_Yards_Allowed = +d.Offensive_Yards_Allowed;
+        d.Year = +d.Year;
+    });
+
+    // Sort countries descending by population
+    data.sort(function(a, b) {
+        return b.Total_Wins - a.Total_Wins;
+    });
+
+    // Append a new SVG area
+    var svg = d3.select("#chart-area-yards").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // X scale
+    var incomeScale = d3.scaleLog()
+        .domain([d3.min(data, function(d){ return d.Offensive_Yards_Allowed }) - 100, d3.max(data, function(d){ return d.Offensive_Yards_Allowed })])
+        .range([0, width]);
+
+    // Y scale
+    var lifeExpectancyScale = d3.scaleLinear()
+        .domain([d3.min(data, function(d){ return d.Total_Wins }) - 5, d3.max(data, function(d){ return d.Total_Wins }) + 5])
+        .range([height, 0]);
+
+    // Radius Scale
+    var populationScale = d3.scaleLinear()
+        .domain(d3.extent(data, function(d){ return d.Total_Wins }))
+        .range([4, 30]);
+
+    // Region Scale (ordinal)
+    var regionScale = d3.scaleOrdinal(d3.schemeCategory10);
 
 
-var xAxis = d3.axisBottom(x);
+    // define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d.Offensive_Yards_Allowed); })
+        .y(function(d) { return y(d.Total_Wins); });
 
-var yAxis = d3.axisLeft(y);
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
 
-//load csv and sort our data
 
-// d3.csv("data/strongandpoorlyperformingteams.csv", function(data) {
-//
-//
-//     data.sort( function(a, b){
-//         return (+b.Turnover_Differential) - (+a.Turnover_Differential);
-//
-//     });
-//
-//     console.log(data);
-//
-//     totalbuildings = data;
-//
-//
-//     //converting strings to numbers
-//
-//     for (var i = 0; i< totalbuildings.length; i++){
-//         totalbuildings[i].Turnover_Differential = +totalbuildings[i].Turnover_Differential;
-//         totalbuildings[i].Total_Wins = +totalbuildings[i].Total_Wins;
-//         totalbuildings[i].number_of_interceptions = +totalbuildings[i].number_of_interceptions;
-//         totalbuildings[i].number_of_interceptions_thrown = +totalbuildings[i].number_of_interceptions_thrown;
-//         totalbuildings[i].number_of_fumbles_lost = +totalbuildings[i].number_of_fumbles_lost;
-//         totalbuildings[i].number_of_fumbles_recovered = +totalbuildings[i].number_of_fumbles_recovered;
-//         totalbuildings[i].Year = +totalbuildings[i].Year;
-//
-//
-//     }
-//
-//
-//     // update our Scale the range of the data in the domains
-//     x.domain(d3.extent(data, function (d) {
-//         return d.Turnover_Differential;
-//     }));
-//     y.domain(data.map(function (d) {
-//         return d.Teams;
-//     }));
-//
-//
-//     svg = d3.select("#chart").append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-// // set up tooltip
-//
-//     var tooltip = d3.select("body").append("div").attr("class", "toolTip");
-//
-//
-//
-//     //add bar graph
-//
-//     svg.selectAll(".bar")
-//         .data(totalbuildings)
-//         .enter()
-//         .append("rect")
-//         .attr("class", function (d) { return "bar bar--" + (d.value < 0 ? "negative" : "positive");})
-//
-//         .attr("fill", function(d){
-//             if(d.Playoff_Made == "TRUE")
-//                 return "LightGreen";
-//             else
-//                 return "LightPink";
-//         })
-//
-//
-//
-//
-//
-//
-//
-//
-//         //.attr("fill", "Plum")
-//         .attr("x", function(d) { return x(Math.min(0, d.Turnover_Differential)); })
-//
-//         .attr("y", function(d) { return y(d.Teams); })
-//         .attr("width", function(d){return Math.abs(x(d.Turnover_Differential) - x(0));})
-//
-//         .attr("height", function(d,i){return y.bandwidth();})
-//         //.attr("class", "barGraph")
-//
-//         .on("mousemove", function(d){
-//             tooltip
-//                 .style("left", d3.event.pageX - 50 + "px")
-//                 .style("top", d3.event.pageY - 70 + "px")
-//                 .style("display", "inline-block")
-//                 .html((d.Teams) + "<br>"  + (d.Turnover_Differential));
-//         })
-//         .on("mouseout", function(d){ tooltip.style("display", "none");})
-//
-//
-//
-//
-//         .on("click", function(d){
-//             console.log(d);
-//             var elt = document.getElementById("instructions");
-//             console.log('elt:' + elt);
-//             if (elt != null) {
-//                 var parent = elt.parentElement;
-//                 parent.removeChild(elt);
-//
-//                 console.log('parent:' + parent);
-//
-//             }
-//
-//             $('#buildingName').text('Team: ' + d.Teams);
-//             $('#height').text('Turnover Differential: ' + d.Turnover_Differential);
-//             $('#season').text('Season: ' + d.Year);
-//             $('#numberofinterceptions').text('Number of Interceptions: ' + d.number_of_interceptions);
-//             $('#numberofinterceptionsthrown').text('Number of Interceptions Thrown: ' + d.number_of_interceptions_thrown);
-//             $('#numberoffumbleslost').text('Number of Fumbles Lost: ' + d.number_of_fumbles_lost);
-//             $('#numberoffumblesrecovered').text('Number of Fumbles Recovered: ' + d.number_of_fumbles_recovered);
-//             $('#playoffmade').text('Whether Madre to Playoff: ' + d.Playoff_Made);
-//
-//
-//             d3.select("#image")
-//                 .select('img').remove();
-//
-//
-//             d3.select("#image")
-//                 .append('img')
-//                 .attr("width", 250)
-//                 .attr("height", 250)
-//                 .attr('src', "data/img/"+d.image);
-//
-//         });
-//
-//
-//
-//     // add the x Axis
-//     svg.append("g")
-//         .attr("transform", "translate(0," + height + ")")
-//         .call(d3.axisBottom(x));
-//
-// // add the y Axis
-//     svg.append("g")
-//         .attr("class", "y axis")
-//         .attr("transform", "translate(" + x(0) + ",0)")
-//         .call(d3.axisRight(y));
-//
-//     //add pics and the click function
-//
-//     svg.on("click", function(d) {
-//         // Do something after click
-//     });
-//
-//
-//     var imgs = svg.selectAll("image").data([0]);
-//     imgs.enter()
-//         .append("svg:image");
-//
-//
-//
-//     function showImage() {
-//         var img = document.getElementById('myImageId');
-//         img.style.visibility = 'visible';
-//     }
-//
-//
-//     var images = [];
-//
-//     svg.selectAll('.Teams')
-//         .data(totalbuildings)
-//         .enter()
-//         .append('image')
-//         .on('click', function(d) {
-//
-//
-//             d3.select("#tool").style("opacity", 1.0);
-//             // d3.select("#tool").html(d.imgsrc);
-//             images.push(d.imgsrc);
-//             d3.select('#tool')
-//                 .selectAll("img")
-//                 .data(images)
-//                 .enter()
-//                 .append('img')
-//                 .attr("width", 50)
-//                 .attr("height", 50)
-//                 .attr('src', function(d, i) {
-//                     return d
-//                 });
-//         });
-//
-//
-// });
+
+
+    // Map data to visual elements (SVG circles)
+    var circles = svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "country-circle")
+        .attr("cx", function(d){ return incomeScale(d.Offensive_Yards_Allowed); })
+        .attr("cy", function(d){ return lifeExpectancyScale(d.Total_Wins); })
+        .attr("r", function(d){ return populationScale(d.Total_Wins); })
+
+        .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html("Team: " + d.Team1 + "<br/>" + "Yards allowed: " + d.Offensive_Yards_Allowed + "<br/>" + "Wins: " + d.Total_Wins)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        })
+        .attr("stroke", "#333")
+        .attr("opacity", 0.7)
+        .attr("fill", function(d){
+            if(d.Playoff_Made === "TRUE")
+                return "LightGreen";
+            else
+                return "LightPink";
+        });
+
+    //.attr("fill", function(d){ return regionScale(d.Team1); });
+
+
+    // //  APPEND TEXT LABELS
+    // var labels = svg.selectAll("text")
+    //     .data(data)
+    //     .enter()
+    //     .append("text")
+    //     .attr("class", "city-label")
+    // 	.text(function(d){ return d.Team1; })
+    //     .attr("x", function(d){ return d.Offensive_Yards_Allowed; })
+    //     .attr("y", function(d){ return d.Total_Wins; });
+
+
+
+    // Add Text Labels
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(function(d) {
+            console.log("hi");
+            console.log(d);
+            // return d[0] + "," + d[1];
+            return d.Team1
+        })
+        .attr("x", function(d){ return d.Offensive_Yards_Allowed; })
+        .attr("y", function(d){ return d.Total_Wins; })
+        // .attr("x", function(d) {
+        //     return incomeScale(d[0]);  // Returns scaled location of x
+        // })
+        // .attr("y", function(d) {
+        //     return lifeExpectancyScale(d[1]);  // Returns scaled circle y
+        // })
+        .attr("font_family", "sans-serif")  // Font type
+        .attr("font-size", "11px")  // Font size
+        .attr("fill", "darkgreen");   // Font color
+
+
+
+
+
+
+
+
+    // Create axes functions
+
+    var xAxis = d3.axisBottom()
+        .scale(incomeScale)
+        .tickFormat(d3.format(",d"))
+        .tickValues([900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 90000, 100000]);
+
+    var yAxis = d3.axisLeft()
+        .scale(lifeExpectancyScale)
+        .ticks(10);
+
+
+    // Append axes to the SVG drawing area
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("y", -15)
+        .attr("x", width)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("offensive yards allowed");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("total wins from 2015");
+
+
+
+});
