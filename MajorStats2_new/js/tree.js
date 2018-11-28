1a4223d3e802eda88dba6261b08ea978d47f1559
curtishsu <curtishsu@Curtiss-MacBook-Pro.local>
2018-11-28 16:44:04 -0500

    add tool tip and arrows and fix spacing on tree

--- MajorStats2_new/js/tree.js
+++ MajorStats2_new/js/tree.js
@@ -10,17 +10,17 @@ var treeData =
             [{"name": "Turnover Differential ≤ 3.5", "gini":0.444, "samples":93, "value":[31,62], "color":"LightPink",
                 "children":[{"name": "Rushing Yards ≤1400.5", "gini":0.262, "samples":58, "value":[9,49], "color":"LightPink",
 
-                    "children":[{"name": "Opponent Offensive Yards ≤ 6112.0", "gini":0.219, "samples":56, "value":[7,49], "color":"LightPink",
+                    "children":[{"name": "Opp. Off. Yards ≤ 6112.0", "gini":0.219, "samples":56, "value":[7,49], "color":"LightPink",
 
-                        "children":[{"name":"Offensive Yards per Play≤ 6.15","gini":0.49, "samples":7, "value":[4,3], "color":"LightPink",
+                        "children":[{"name":"Off. Yards per Play≤ 6.15","gini":0.49, "samples":7, "value":[4,3], "color":"LightPink",
 
                             "children":[{"gini":0.0, "samples":3, "value":[0,3],"color":"LightPink"},
                                 {"gini":0.0, "samples":4, "value":[4,0],"color":"LightGreen"}]
                         },
 
-                            {"name":"Offensive Yards per Play≤ 4.8", "gini":0.115, "samples":49, "value":[3,46], "color":"LightGreen",
+                            {"name":"Off. Yards per Play≤ 4.8", "gini":0.115, "samples":49, "value":[3,46], "color":"LightGreen",
 
-                                "children":[{"name":"Opponent Red Zone Percentage ≤ 35.55", "gini":0.08, "samples":48, "value":[2,46],"color":"LightPink",
+                                "children":[{"name":"Opp. Red Zone Perc. ≤ 35.55", "gini":0.08, "samples":48, "value":[2,46],"color":"LightPink",
 
                                     "children":[{
                                         // "name":"Passing Yards_x ≤ 3136.0",
@@ -46,10 +46,10 @@ var treeData =
                         {"gini":0.0, "samples":2, "value":[2,0], "color":"LightGreen"}]
                 },
 
-                    {"name": "Opponent Offensive Yards ≤ 5981.0", "gini":0.467, "samples":35, "color":"LightGreen",
+                    {"name": "Opp. Off. Yards ≤ 5981.0", "gini":0.467, "samples":35, "color":"LightGreen",
                         "children":[{"gini":0.0, "samples":11, "value":[11,0], "color":"LightPink"},
 
-                            {"name": "Opponent Offensive Yards ≤ 5641.0", "gini":0.497, "samples":24, "value":[11,13], "color":"LightGreen",
+                            {"name": "Opp. Off. Yards ≤ 5641.0", "gini":0.497, "samples":24, "value":[11,13], "color":"LightGreen",
 
                                 "children":[{"gini":0.0, "samples":6, "value":[0,6],"color":"LightPink"},
                                     {"name":"Times Sacked_x ≤ 30.0", "gini":0.475, "sample":18, "value":[11,7], "color":"LightGreen",
@@ -93,7 +93,7 @@ var treeData =
 
             },
 
-                {"name": "Opponent Offensive Yards ≤ 4696", "gini":0.3, "samples":163, "value":[133, 30], "color":"LightGreen",
+                {"name": "Opp. Off. Yards ≤ 4696", "gini":0.3, "samples":163, "value":[133, 30], "color":"LightGreen",
 
                     "children":[{"name": "Rushing Yards ≤2529.5", "gini":0.223, "samples":149, "value":[130, 19],"color":"LightPink",
 
@@ -104,15 +104,15 @@ var treeData =
                                 "children":[{"name":"Rushing Yards_x ≤ 1425.0", "gini":0.32, "samples":65, "value":[52,13], "color":"LightPink",
 
                                     "children":[{
-                                        // "name":"Opponent Times Sacked_x ≤ 20.5",
+                                        // "name":"Opp. Times Sacked_x ≤ 20.5",
                                         "gini":0.278, "samples":60, "value":[50,10], "color":"LightPink",
 
                                         // "children":[{
-                                        // "name":"Opponent Red Zone Percentage ≤ 56.15",
+                                        // "name":"Opp. Red Zone Perc. ≤ 56.15",
                                         //     "gini":0.259, "samples":59, "value":[50,9], "color":"LightPink",
 
 
-                                        // "children":[{"name":"Opponent Times Sacked_x ≤ 39.0", "gini":0.444, "samples":15, "value":[10,5], "color":"LightPink",
+                                        // "children":[{"name":"Opp. Times Sacked_x ≤ 39.0", "gini":0.444, "samples":15, "value":[10,5], "color":"LightPink",
                                         //
                                         //     "children":[{"name":"Penalty Yards ≤ 994.0", "gini":0.32, "samples":5, "value":[1,4], "color":"LightPink",
                                         //
@@ -145,7 +145,7 @@ var treeData =
                                         //     {"name": "Passing Yards_x ≤ 3046.0","gini":"0.165", "samples":44, "value":[40,4], "color":"LightGreen",
                                         //
                                         //
-                                        //         "children":[{"name":"Thrid Down Percentage ≤ 44.05","gini":"0.059", "samples":33, "value":[32,1], "color":"LightPink",
+                                        //         "children":[{"name":"Thrid Down Perc. ≤ 44.05","gini":"0.059", "samples":33, "value":[32,1], "color":"LightPink",
                                         //
                                         //
                                         //             "children":[{"name":"Turnover Differential ≤ -1.0","gini":"0.5", "samples":2, "value":[1,1], "color":"LightPink",
@@ -191,7 +191,7 @@ var treeData =
 
                                     },
 
-                                        {"name":"Offensive Yards Per Play ≤ 5.1","gini":"0.48", "samples":5, "value":[2,3], "color":"LightGreen",
+                                        {"name":"Off. Yards Per Play ≤ 5.1","gini":"0.48", "samples":5, "value":[2,3], "color":"LightGreen",
                                             "children":[{"gini":0.0, "samples":3, "value":[0,3],"color":"LightPink"},
                                                 {"gini":0.0, "samples":2, "value":[2,0],"color":"LightGreen"}]
 
@@ -200,16 +200,16 @@ var treeData =
 
                                 },
 
-                                    {"name":"Opponent Red Zone Percentage ≤ 50.95", "gini":0.071, "samples":81, "value":[78,3],"color":"LightGreen",
+                                    {"name":"Opp. Red Zone Perc. ≤ 50.95", "gini":0.071, "samples":81, "value":[78,3],"color":"LightGreen",
 
 
 
                                         "children":[{"gini":0.0, "samples":60, "value":[60,0],"color":"LightPink"},
 
 
-                                            {"name":"Opponent Offensive Yards ≤ 5268.0", "gini":0.245, "samples":21, "value":[18,3],"color":"LightGreen",
+                                            {"name":"Opp. Off. Yards ≤ 5268.0", "gini":0.245, "samples":21, "value":[18,3],"color":"LightGreen",
 
-                                                "children":[{"name":"Third Down Percentage ≤ 42.4", "gini":0.18, "samples":20, "value":[18,2],"color":"LightPink",
+                                                "children":[{"name":"Third Down Perc. ≤ 42.4", "gini":0.18, "samples":20, "value":[18,2],"color":"LightPink",
 
 
                                                     "children":[{"gini":0.0, "samples":1, "value":[0,1],"color":"LightPink"},
@@ -250,7 +250,7 @@ var treeData =
 
                             "children":[{"gini":0.0, "samples":10, "value":[0,10],"color":"LightPink"},
 
-                                {"name":"Opponent Offensive Yards ≤ 4380.5", "gini":0.375, "samples":4, "value":[3,1],"color":"LightGreen",
+                                {"name":"Opp. Off. Yards ≤ 4380.5", "gini":0.375, "samples":4, "value":[3,1],"color":"LightGreen",
 
                                     "children":[{"gini":0.0, "samples":3, "value":[3,0],"color":"LightPink"},
 
@@ -277,8 +277,8 @@ var treeData =
 
 
 // Set the dimensions and margins of the diagram
-var margin = {top: 20, right: 90, bottom: 30, left: 150},
-    width = 960 - margin.left - margin.right,
+var margin = {top: 0, right: 90, bottom: 0, left: 160},
+    width = 1000 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -292,54 +292,61 @@ var svg = d3.select("#tree").append("svg")
         + margin.left + "," + margin.top + ")");
 
 // create legend
-svg
+var svgTree = d3.select("#treeLegend").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", 150)
+    .append("g")
+    .attr("transform", "translate("
+        + margin.left + "," + margin.top + ")");
+var TreeLegendSmudge = 20;
+svgTree
     .append("text")
     // .attr("class", "treeLegend")
     .attr("x", 0)
-    .attr("y", 0)
+    .attr("y", 10 + TreeLegendSmudge)
     .attr("class", "treeLegendLabel")
     .text("Legend")
     .style("font-weight", "bold")
     .style("font-size", "16px")
-svg
+svgTree
     .append("rect")
     // .attr("class", "treeLegend")
     .attr("x", 0)
-    .attr("y", 20)
+    .attr("y", 20+ TreeLegendSmudge)
     .attr("fill", "LightPink")
     .attr("width", 20)
     .attr("height", 20);
 
-svg
+svgTree
     .append("text")
     // .attr("class", "treeLegend")
     .attr("x", 30)
-    .attr("y", 35)
+    .attr("y", 35+ TreeLegendSmudge)
     .text("Majority of teams in node missed playoffs")
 
 
 
-svg
+svgTree
     .append("rect")
     // .attr("class", "treeLegend")
     .attr("x", 0)
-    .attr("y", 50)
+    .attr("y", 50+ TreeLegendSmudge)
     .attr("fill", "LightGreen")
     .attr("width", 20)
     .attr("height", 20);
 
-svg
+svgTree
     .append("text")
     // .attr("class", "treeLegend")
     .attr("x", 30)
-    .attr("y", 65)
+    .attr("y", 65+ TreeLegendSmudge)
     .text("Majority of teams in node made playoffs")
 
-svg
+svgTree
     .append("text")
     // .attr("class", "treeLegend")
     .attr("x", 0)
-    .attr("y", 100)
+    .attr("y", 100+ TreeLegendSmudge)
     .text("[x, y]: x teams in node did not make playoffs, y teams made playoffs")
     // .attr("fill", "LightGreen")
     // .attr("width", 20)
@@ -349,9 +356,10 @@ svg
 // create arrows and lines
 var smudge = 55
 svg.append("line")
+    .attr("class", "line1")
     .attr("x1", -150)
     .attr("y1", height/2 - 80 - smudge)
-    .attr("x2", -100)
+    .attr("x2", -75)
     .attr("y2", height/2 - 120 - smudge)
     .attr("stroke-width", 1)
     .attr("stroke", "black")
@@ -370,12 +378,13 @@ svg.append("svg:defs").append("svg:marker")
 
 svg
     .append("text")
-    // .attr("class", "treeLegend")
+    .attr("class", "treeFalse")
     .attr("x", -150)
     .attr("y", height/2 - 170)
     .text("False");
 
 svg.append("line")
+    .attr("class", "line2")
     .attr("x1", -150)
     .attr("y1", height/2 + 80 + smudge)
     .attr("x2", -100)
@@ -396,7 +405,7 @@ svg.append("svg:defs").append("svg:marker")
     .style("fill", "black");
 svg
     .append("text")
-    // .attr("class", "treeLegend")
+    .attr("class", "treeTrue")
     .attr("x", -150)
     .attr("y", height/2 + 170)
     .text("True");
@@ -465,24 +474,38 @@ function update(source) {
 
     // Add labels for the nodes
     nodeEnter.append('text')
-        .attr("dy", ".35em")
+        .attr("dy", ".1em")
+        // .attr("dy", function(d){
+            // return "12px";}
+        //     console.log(d)
+        //     if (d.height%2 ===0){
+        //         console.log("top")
+        //         return "-1em"
+        //     }
+        //     else{
+        //         console.log("bottom")
+        //         return "1em"
+        //     }
+        // })
         .attr("x", function(d) {
-            return d.children || d._children ? -13 : 13;
+            return d.children || d._children ? -20 : 13;
         })
         .attr("text-anchor", function(d) {
             return d.children || d._children ? "end" : "start";
         })
         .text(function(d) {
             if (d.data.name){
-                console.log("What")
+                // console.log("What")
                 return d.data.name;
             }
             else{
-                console.log("yeah")
+                // console.log("yeah")
                 return fixStr(d.data.value);
             }
 
-        });
+        })
+        .attr("border", "3px")
+        // .style("fill-opacity", 1e-6);
 
     // UPDATE
     var nodeUpdate = nodeEnter.merge(node);
@@ -502,7 +525,25 @@ function update(source) {
             return d.data.color;
         })
         .attr('cursor', 'pointer');
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
+    // create mouse controls
+    nodeEnter.on("mouseover", function(d){
+        console.log(d)
+        div.transition()
+            .duration(200)
+            .style("opacity", 1);
+        div	.html("Samples in Node: " + d.data.samples + "</br>" + "Playoff split: " + "[" +d.data.value + "] </br>" +  "Gini: " + d.data.gini)
+            .style("left", (d3.event.pageX - 69 ) + "px")
+            .style("top", (d3.event.pageY - 69 - 10) + "px");
+    })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(100)
+                .style("opacity", 0);
+        });
 
     // Remove any exiting nodes
     var nodeExit = node.exit().transition()
@@ -557,6 +598,63 @@ function update(source) {
         d.y0 = d.y;
     });
 
+    // console.log(nodes[0])
+
+    var moveLine = 50;
+
+    var line1 = svg.selectAll(".line1")
+
+    line1.enter().append()
+        .attr("class", "line1")
+        .merge(line1)
+        .transition()
+        .duration(duration)
+        .attr("y1", nodes[0].x - 25)
+        .attr("y2", nodes[0].x - 100)
+        .attr("x1", -125)
+        .attr("x2", -50);
+
+    line1.exit().remove()
+
+    var text1 = svg.selectAll(".treeFalse")
+
+    text1.enter().append()
+        .attr("class", "textFalse")
+        .merge(text1)
+        .transition()
+        .duration(duration)
+        .attr("y", nodes[0].x - 100)
+        .attr("x", -120);
+
+    text1.exit().remove()
+
+
+    var line2 = svg.selectAll(".line2")
+
+    line2.enter().append()
+        .attr("class", "line12")
+        .merge(line2)
+        .transition()
+        .duration(duration)
+        .attr("y1", nodes[0].x + 25)
+        .attr("y2", nodes[0].x + 100)
+        .attr("x1", -125)
+        .attr("x2", -50);
+
+    line2.exit().remove()
+
+    var text2 = svg.selectAll(".treeTrue")
+
+    text2.enter().append()
+        .attr("class", "textTrue")
+        .merge(text2)
+        .transition()
+        .duration(duration)
+        .attr("y", nodes[0].x + 100)
+        .attr("x", -120);
+
+    text2.exit().remove()
+
     // Creates a curved (diagonal) path from parent to the child nodes
     function diagonal(s, d) {
 
@@ -581,13 +679,13 @@ function update(source) {
     }
 
     function fixStr(s){
-        console.log(s)
-        console.log(typeof(s))
+
+
         var x = s[0];
         var y = s[1];
 
         var out = "[" + x + "," + " " + y + "]";
-        console.log(out)
+        // console.log(out)
         return out;
     }
 }
